7da70c1e8ac60da592ce7482c969017423e77152
Rakib Hassan <mailrakibhassan@gmail.com>
2025-06-09 16:27:25 +0600

    (fix) not exposing EXTERNAL_BASE_API to client side

--- src/config/envConfig.ts
+++ src/config/envConfig.ts
@@ -1,6 +1,6 @@
 export const envConfig = {
   baseApi: process.env.NEXT_PUBLIC_BASE_API,
-  externalBaseApi: process.env.NEXT_PUBLIC_EXTERNAL_BASE_API,
+  externalBaseApi: process.env.EXTERNAL_BASE_API,
   algolia_app_id: process.env.NEXT_PUBLIC_ALGOLIA_APP_ID,
   algolia_search_api_key: process.env.NEXT_PUBLIC_ALGOLIA_SEARCH_API_KEY,
 };
